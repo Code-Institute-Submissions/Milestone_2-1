d3df846d52724ff8e6324e038801284088674b41
CojanRadu <raduacojan@gmail.com>
2020-03-05 21:08:28 +0000

    added player pieces

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,30 +83,42 @@ console.log(hexToPixel(testHex));
 
 function makeHexagonalShape(N) {
     let results = [];
-     for (let q = -N; q <= N; q++) {
-    
-        
+    for (let q = -N; q <= N; q++) {
+
+
         // for (let r = -N; r <= N; r++) {
         for (let r = N; r >= -N; r--) {
             let hex = new HexCube(q + N, r + N, -q - r + N);
 
-            let temp1 = q + N;
-            let temp2 = -q - r + N;
+            let axialQ = q + N;
+            let axialR = -q - r + N;
 
-            let hex2 = new HexAxial(r + N, -q - r + N);
+
+            let hex2 = new HexAxial(axialQ, axialR);
 
             coord = hexToPixel(hex2);
 
             if (((Math.abs(q) + Math.abs(r) + Math.abs(-q - r)) / 2) <= N) {
                 results.push(hex);
                 //results.push(hex2);
-                $("#grid").append(`<div class='hex' style="top:${coord.x}px; left:${coord.y}px">${temp2}xxxxxxxxx${temp1}</div>`);
+                $("#grid").prepend(`<div class='hex' style="top:${coord.x}px; left:${coord.y}px">${axialR}xxxxxxxxx${axialQ}</div>`);
+
+                if ((axialQ == 0 || axialQ == 1)) {
+                    $("#grid div").first().append(`<div class='cylinder playerOne' axial="${axialR} ${axialQ}"></div>`);
+                }
+                else if ((axialQ == 2 * N) || (axialQ == 2 * N - 1)) {
+                    $("#grid div").first().append(`<div class='cylinder playerTwo' axial="${axialR} ${axialQ}"></div>`);
+                }
+
             }
 
-            else { $("#grid").prepend(`<div class='hex test' style="top:${coord.x}px; left:${coord.y}px">${temp2}xxxxxxxxx${temp1}</div>`); }
+            else { $("#grid").append(`<div class='hex test' style="top:${coord.x}px; left:${coord.y}px">${axialR}xxxxxxxxx${axialQ}</div>`); }
 
-            // console.log(temp2, temp1);
+            // console.log(axialR, axialQ);
         }
+
+
+
 
 
     }
@@ -115,19 +127,43 @@ function makeHexagonalShape(N) {
 
 console.log(makeHexagonalShape(2));
 
+var timer;
 
-// function makeHexagonalShape(N) {
-//     let results = [];
-//     for (let q = 0; q <= N; q++) {
-//         for (let r = 0; r <= N; r++) {
-//             let hex = new HexCube(q, r, -q-r);
-//             if (((Math.abs(q) + Math.abs(r) + Math.abs(-q-r)) / 2) <= N) 
-//             {
-//                 results.push(hex);
-//             }
-//         }
-//     }
-//     return results;
-// }
 
-//  console.log(makeHexagonalShape(6));
+$(".cylinder").click(function () {
+    console.log($(this).attr("axial"));
+    blinking($(this));
+    $(this).addClass("animateArc");
+    //TODO FIX ZINDEX
+});
+
+
+function blinking(elm) {
+    timer = setInterval(blink, 200);
+    function blink() {
+        $(elm).fadeTo(200, 0.9, function () { $(elm).fadeTo(200, 1.0); });
+    }
+}
+
+
+// ///try to change css on the fly
+// var setRotation = function($target, degrees) {
+//     $target.css("-webkit-transform", "rotate(" + degrees + "deg)"); /* Safari and Chrome */
+//     $target.css("-moz-transform", "rotate(" + degrees + "deg)"); /* Firefox */
+//     $target.css("-ms-transform", "rotate(" + degrees + "deg)"); /* IE 9 */
+//     $target.css("-o-transform", "rotate(" + degrees + "deg)"); /* Opera */
+//     $target.css("transform", "rotate(" + degrees + "deg)"); 
+//   };
+  
+//   var rotation = 0;
+  
+//   //Set css() of elements
+//   $( document ).ready(function(){
+//     setInterval(function() {
+//       rotation += 0.1;
+//       setRotation($(".rotation"), rotation); 
+//     }, 1000/60);
+//   });
+
+// TODO Add 1 oor 2 rows of player puck
+//TODO add green pieces count for players
